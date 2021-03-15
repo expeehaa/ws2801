b7aea083b0ef579728174aea4954d52d6a33430f
expeehaa <expeehaa@outlook.com>
2021-03-15 17:47:40 +0100

    Rename let binding.

--- spec/lib/ws2801/instance_spec.rb
+++ spec/lib/ws2801/instance_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 RSpec.describe WS2801::Instance do
-	let(:instance) { WS2801::Instance.new }
+	let(:default_instance) { WS2801::Instance.new }
 	
 	describe '.new' do
 		it 'accepts parameters' do
@@ -16,72 +16,72 @@ RSpec.describe WS2801::Instance do
 	
 	describe '#length' do
 		it 'has a default value' do
-			expect(instance.length).to eq 25
+			expect(default_instance.length).to eq 25
 		end
 		
 		it 'can be set' do
-			instance.length = 20
+			default_instance.length = 20
 			
-			expect(instance.length).to eq 20
+			expect(default_instance.length).to eq 20
 		end
 	end
 	
 	describe '#strip' do
 		it 'has a default value' do
-			expect(instance.strip).to eq []
+			expect(default_instance.strip).to eq []
 		end
 		
 		it 'can be set' do
-			instance.strip = [0,0,0,0]
+			default_instance.strip = [0,0,0,0]
 			
-			expect(instance.strip).to eq [0,0,0,0]
+			expect(default_instance.strip).to eq [0,0,0,0]
 		end
 	end
 	
 	describe '#device' do
 		it 'has a default value' do
-			expect(instance.device).to eq '/dev/spidev0.0'
+			expect(default_instance.device).to eq '/dev/spidev0.0'
 		end
 		
 		it 'can be set' do
-			instance.device = '/dev/spidev0.1'
+			default_instance.device = '/dev/spidev0.1'
 			
-			expect(instance.device).to eq '/dev/spidev0.1'
+			expect(default_instance.device).to eq '/dev/spidev0.1'
 		end
 	end
 	
 	describe '#autowrite' do
 		it 'has a default value' do
-			expect(instance.autowrite).to eq true
+			expect(default_instance.autowrite).to eq true
 		end
 		
 		it 'can be set' do
-			instance.autowrite = false
+			default_instance.autowrite = false
 			
-			expect(instance.autowrite).to eq false
+			expect(default_instance.autowrite).to eq false
 		end
 	end
 	
 	describe '#generate' do
 		it 'generates strip values' do
-			instance.generate
+			default_instance.generate
 			
-			expect(instance.strip.length).to eq 3*25+1
-			expect(instance.strip       ).to all(be 0)
+			expect(default_instance.strip.length).to eq 3*25+1
+			expect(default_instance.strip       ).to all(be 0)
 		end
 		
 		it 'accepts a parameter "only_if_empty"' do
-			instance.length = 2
+			default_instance.length = 2
 			
-			expect{instance.generate(only_if_empty: false)}.    to change{instance.strip}.from([]).to([0,0,0,0,0,0,0])
+			expect{default_instance.generate(only_if_empty: false)}.    to change{default_instance.strip}.from([]).to([0,0,0,0,0,0,0])
 			
-			instance.length = 1
+			default_instance.length = 1
 			
-			expect{instance.generate(only_if_empty: true )}.not_to change{instance.strip}
+			expect{default_instance.generate(only_if_empty: true )}.not_to change{default_instance.strip}
 			
-			instance.strip = []
+			default_instance.strip = []
 			
-			expect{instance.generate(only_if_empty: true )}.    to change{instance.strip}.from([]).to([0,0,0,0])
+			expect{default_instance.generate(only_if_empty: true )}.    to change{default_instance.strip}.from([]).to([0,0,0,0])
 		end
 	end
 end
